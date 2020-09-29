6687858bb0b25429cabe6fd86beb65df8ac7ac4c
ado87 <63777512+ado87@users.noreply.github.com>
2020-09-29 18:34:54 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,47 +1,15 @@
-$(function(){
-    $('.popup > table > tbody tr td a b font').on('click', function(){
-        var name = $(this).attr('font');
-        console.log(name)
 
-        if(name !== '#') {
-            $('.popup').fadeOut(200);
-        }
-        return false;
-    })
-})
-$(function(){
-    $('.popup2 > table > tbody tr td a b font').on('click', function(){
-        var name = $(this).attr('font');
-        console.log(name)
+$(document).ready( function(){
+   $(window).scroll(function(){
+    if($(this).scrollTop() > 900) {
+        $('.link').fadeIn();
+    } else {
+        $('.link').fadeOut();
+    }
+    });
 
-        if(name !== '#') {
-            $('.popup2').fadeOut(200);
-        }
+    $('.link').click(function() {
+        $('html, body').animate( {scrollTop : 0 }, 600);
         return false;
-    })
-})
-$(function(){
-    $('.popup3 > table > tbody tr td a b font').on('click', function(){
-        var name = $(this).attr('font');
-        console.log(name)
-
-        if(name !== '#') {
-            $('.popup3').fadeOut(200);
-        }
-        return false;
-    })
-})
-// function setcookie( name, value, expirehours ) {
-//     var todayDate = new Date();
-//     todayDate.setHours( todayDate.getHours() + expirehours );
-//     document.cookie = name + "=" + escape( value ) + "; path=/; expires=" + todayDate.toGMTString() + ";"
-//     }
-    
-//     function closeWin() {
-//     if ( document.notice_form.chkbox.checked ){
-//     setcookie( "maindiv", "done" , 24 );
-//     }
-//     document.all['popup'].style.display = "none";
-//     }
-    
-    
+    }); 
+});
